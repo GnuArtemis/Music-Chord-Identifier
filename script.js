95fbb1181273317446c14d2b18941162319cf5c4
caitlinbou <caitlin.bouroncle@gmail.com>
2020-09-22 14:08:25 -0700

    empty click event created for each key

--- script.js
+++ script.js
@@ -1,7 +1,48 @@
 //API Documentation: (no key required)   https://api.uberchord.com/
-$.ajax({
-    url:"http://cors-anywhere.herokuapp.com/www.tofret.com/reverse-chord-finder.php?return-type=json&notes=A+F%23+Eb",
-    method: "GET"
-}).then(function(response){
-    console.log(response);
+// $.ajax({
+//     url:"http://cors-anywhere.herokuapp.com/www.tofret.com/reverse-chord-finder.php?return-type=json&notes=A+F%23+Eb",
+//     method: "GET"
+// }).then(function(response){
+//     console.log(response);
+// })
+
+// TODO: create click events for a, aSh, b, c, cSh, d, dSh, e, f, fSh, g, gSh
+// A, A#, B, C, C#, D, D#, E, F, F#, G, G#
+
+$("#a").on("click", function(){
+
 })
+$("#aSh").on("click", function(){
+
+})
+$("b").on("click", function(){
+
+})
+$("#c").on("click", function(){
+   
+})
+$("#cSh").on("click", function(){
+
+})
+$("#d").on("click", function(){
+  
+})
+$("#dSh").on("click", function(){
+
+})
+$("#e").on("click", function(){
+
+})
+$("#f").on("click", function(){
+
+})
+$("#FSh").on("click", function(){
+  
+})
+$("#g").on("click", function(){
+
+})
+$("#gSh").on("click", function(){
+  
+})
+
