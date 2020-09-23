bf88a442dfba0957da887b3824836a4c981f4fb1
Ann Guinee <annmguinee@gmail.com>
2020-09-23 12:21:15 -0700

    Can now use an API call to correctly identify 3-note static inputs

--- script.js
+++ script.js
@@ -1,7 +1,85 @@
-//API Documentation: (no key required)   https://api.uberchord.com/
+//API Documentation: (no key required)   www.tofret.com/
+var first_note = "B";
+var second_note = "G";
+var third_note = "E";
+var numNotes = 3;
+
+"https://cors-anywhere.herokuapp.com/www.tofret.com/reverse-chord-finder.php?return-type=json&notes=" + "A+B+C"
+
 $.ajax({
-    url:"http://cors-anywhere.herokuapp.com/www.tofret.com/reverse-chord-finder.php?return-type=json&notes=A+F%23+Eb",
+    url: `https://cors-anywhere.herokuapp.com/www.tofret.com/reverse-chord-finder.php?return-type=json&notes=${first_note}+${second_note}+${third_note}`,
     method: "GET"
-}).then(function(response){
+}).then(function (response) {
+    // console.log("hello world")
+    response = JSON.parse(response);
+
     console.log(response);
+
+    findExactFit(response);
+
+
+
+    // console.log
 })
+
+function findExactFit(response) {
+    // var newList = Object.entries(response.chords);
+    // console.log(newList);
+
+    
+    for (const property in response.chords) {
+        for (const key in response.chords[property]) {
+            console.log(`${property}: ${key}, ${response.chords[property][key]}`)
+            if(response.chords[property][key].includes(first_note)&&response.chords[property][key].includes(second_note)&&response.chords[property][key].includes(third_note)&&response.chords[property][key].length===5) {
+                console.log("HIT!!!");
+                console.log(`${property} ${key}`);
+            }
+        }
+    }
+
+    // newList.forEach(function (element, index) {
+    //     var justChord = Object.values(element[1]);
+    //     console.log(justChord);
+    //     // justChord.forEach(e => {
+    //     //     if (e === `${first_note} ${second_note} ${third_note}`) {
+    //     //         console.log("Hit!")
+    //     //         console.log(Object.keys(element[1])[index]);
+    //     //         // return [e, Object.keys(element[1])[index]];
+    //     //     }
+    //     // });
+
+
+    // });
+}
+
+
+// $.ajax({
+//     url:"https://cors-anywhere.herokuapp.com/www.tofret.com/reverse-chord-finder.php?return-type=json&notes=A+F%23+D",
+//     method: "GET"
+// }).then(function(response){
+//     // console.log("hello world")
+//     response=JSON.parse(response);
+
+//     console.log(response);
+
+//     // if(first_note === "A") {
+
+//     // }
+// })
+
+// $.ajax({
+//     url:"https://cors-anywhere.herokuapp.com/www.tofret.com/reverse-chord-finder.php?return-type=json&notes=G+B",
+//     method: "GET"
+// }).then(function(response){
+//     // console.log("hello world")
+//     response=JSON.parse(response);
+
+//     console.log(response);
+
+//     // if(first_note === "A") {
+
+//     // }
+// })
+
+
+// 
