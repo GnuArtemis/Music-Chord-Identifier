47e62053f3c5bb218af1ae432ab69e14d0a6bc62
caitlinbou <caitlin.bouroncle@gmail.com>
2020-09-24 13:35:24 -0700

    pull updates, commit new files

---
+++ aboutUs.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('.carousel').carousel();
+  });
+      
+$(document).on("click", ".name" )
+
+  // $(".caitlin").on("click", function{
+
+// })
+$(".name")
+$(".team")
+$(".caitlin")
+$(".matt") 
+$(".ann")
+$(".petar")
+$(".scott") 
+
+$(".profile")
+$(".blurb")
+$(".github")
+$(".linkedin")
