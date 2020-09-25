e6d5b83db76cc26249b3ef824e35be663b725d78
caitlinbou <caitlin.bouroncle@gmail.com>
2020-09-25 11:37:52 -0700

    added team photo option updated my photo

--- aboutUs.js
+++ aboutUs.js
@@ -33,11 +33,11 @@ $(document).ready(function () {
       name: "Ann Guinee",
       blurb: "I am a musician and a mathematician and the Concept Creator of this project. I love to keep my brain working, and am a strong leader.",
       gitHubLink: {
-        url: "#",
+        url: "https://github.com/GnuArtemis",
         linkLable: "Ann's Github",
       },
       linkedInLink: {
-        url: "#",
+        url: "https://www.linkedin.com/in/ann-guinee-b8186718b/",
         linkLable: "Ann's LinkedIn",
       },
     },
@@ -46,7 +46,7 @@ $(document).ready(function () {
       name: "Matt Weber",
       blurb: "I am a wizard with this coding biz already, and can get you out of many sticky situations. Hire me now! And I have a kid.",
       gitHubLink: {
-        url: "#",
+        url: "https://github.com/webermg",
         linkLable: "Matt's Github",
       },
       linkedInLink: {
@@ -59,11 +59,11 @@ $(document).ready(function () {
       name: "Petar Zivkovic",
       blurb: "I have been in construction for years, am always eager to help, and can not WAIT to move into a new career in coding.",
       gitHubLink: {
-        url: "#",
+        url: "https://github.com/Petar85",
         linkLable: "Petar's Github",
       },
       linkedInLink: {
-        url: "#",
+        url: "https://www.linkedin.com/in/petar-zivkovic-6b88ba1b3/",
         linkLable: "Petar's LinkedIn",
       },
     },
@@ -72,7 +72,7 @@ $(document).ready(function () {
       name: "Scott Dancer",
       blurb: "I am an artist who lives at the best beach in the Northwest, and love a good challenge. I also make a mean cocktail, but can't wait to be a developer instead.",
       gitHubLink: {
-        url: "#",
+        url: "https://github.com/ScottDancer",
         linkLable: "Scott's Github",
       },
       linkedInLink: {
@@ -98,57 +98,3 @@ $(document).ready(function () {
     },
   });
 });
-//   // $("#team")
-//   $(".profile").text("Our Team");
-//   $(".blurb").text("What we are about");
-//   $(".github").text("Project Github");
-//   $(".github").attr(
-//     "href",
-//     "https://github.com/GnuArtemis/Music-Chord-Identifier"
-//   );
-//   $(".linkedin").text("");
-//   $(".linkedin").attr("href", "#");
-
-//   if ($("#caitlin").hasClass("active")) {
-//     $(".profile").text("Caitlin Bouroncle");
-//     $(".blurb").text("Hi I'm Caitlin");
-//     $(".github").text("Caitlin's Github");
-//     $(".github").attr("href", "https://github.com/caitlinbou");
-//     $(".linkedin").text("Caitlin's LinkedIn");
-//     $(".linkedin").attr(
-//       "href",
-//       "https://www.linkedin.com/in/caitlin-bouroncle-185b75182/"
-//     );
-//   }
-
-//   // $("#ann")
-//   $(".profile").text("Ann Guinee");
-//   $(".blurb").text("Hi I'm Ann");
-//   $(".github").text("Ann's Github");
-//   $(".github").attr("href", "#");
-//   $(".linkedin").text("Ann's LinkedIn");
-//   $(".linkedin").attr("href", "#");
-
-//   // $("#matt")
-//   $(".profile").text("Matt Weber");
-//   $(".blurb").text("Hi I'm Matt");
-//   $(".github").text("Matt's Github");
-//   $(".github").attr("href", "#");
-//   $(".linkedin").text("Matt's LinkedIn");
-//   $(".linkedin").attr("href", "#");
-
-//   // $("#petar")
-//   $(".profile").text("Petar Zivkovic");
-//   $(".blurb").text("Hi I'm Petar");
-//   $(".github").text("Petar's Github");
-//   $(".github").attr("href", "#");
-//   $(".linkedin").text("Petar's LinkedIn");
-//   $(".linkedin").attr("href", "#");
-
-//   // $("#scott")
-//   $(".profile").text("Scott Dancer");
-//   $(".blurb").text("Hi I'm Scott");
-//   $(".github").text("Scott's Github");
-//   $(".github").attr("href", "#");
-//   $(".linkedin").text("Scott's LinkedIn");
-//   $(".linkedin").attr("href", "#");
